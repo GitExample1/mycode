7b7abb1cf74d6487766adb02a01fc46cb041c6d8
GitExample1 <Gitnicolash@gmail.com>
2023-05-26 15:40:43 +0000

    Playing with python pip packages

---
+++ color/color3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+import crayons
+
+
+def main():
+
+    # prints name in red
+    print(crayons.red("nicolas"))
+
+    # prints name in blue
+    print (crayons.blue("nicolas"))
+
+    # prints name in magenta
+    print (crayons.magenta("nicolas"))
+if __name__ == "__main__":
+    main()
