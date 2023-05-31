c407f526b990fa137a7e61467ec722ef330227c4
GITExample1 <Gitnicolash@gmail.com>
2023-05-31 21:56:30 +0000

    Miniproject02 update

--- miniproject02/find_comic_char.py
+++ miniproject02/find_comic_char.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python3
+"""
+Author: Nicolas Herrera
+A program to find a comic book character and the date they first appeared
+"""
 
-# imports
-import csv
-
-# open comic-characters data
-with open("comic-characters.csv", "r") as comic_char:
-    # find character comic and when they first appeared
-    print("Find character comic and when they first appeared")
-    character = input(f"Enter name of character:\n")
-    for line in comic_char:
-
-        if character in line:
-            line = line.split(",")
-            name = line[0]
-            comic = line[2]
-            first_date = line[12] + line[13]
-            print(f"{name}, apart of {comic}, first appeared in {first_date}.")
-
+def main():
+    # open comic-characters data
+    with open("comic-characters.csv", "r",errors='replace' ) as comic_char:
+        # find comic book character and when they first appeared
+        print("Search comic book character and the date when they first appeared")
+        character = input(f"Enter name of character:\n")
+        for line in comic_char:
+            if character in line:
+                line = line.split(",")
+                name = line[0]
+                comic = line[1]
+                reality = line[2]
+                first_date = line[12] + line[13]
+                # gives comment if no data availible for first_date
+                if line[12] == "":
+                    first_date = "data not found"
+                
+                print(f"{name}, apart of {comic} in {reality}, first appeared in {first_date}.")
+    
+if __name__ == "__main__":
+    main()
 
 
 
